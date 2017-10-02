8f89f4898cda7b7544321ff8dda2f76ea22d4c58
Alec Brunelle <alec@alec.coffee>
2017-10-02 13:58:16 -0400

    init

---
+++ lib/community_web/resolvers/news_resolver.ex
@@ -0,0 +1,18 @@
+defmodule CommunityWeb.NewsResolver do
+  alias Community.News
+
+  def all_links(_root, _args, _info) do
+    links = News.list_links()
+    {:ok, links}
+  end
+
+  def create_link(_root, args, _info) do
+    # TODO: add detailed error message handling later
+    case News.create_link(args) do
+      {:ok, link} ->
+        {:ok, link}
+      _error ->
+        {:error, "could not create link"}
+    end
+  end
+end
